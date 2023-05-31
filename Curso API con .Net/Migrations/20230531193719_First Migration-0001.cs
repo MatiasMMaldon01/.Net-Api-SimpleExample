b9f5439f345c840939220c3f3318d7de22abb247
MatiasMMaldon01 <matiasm843@gmail.com>
2023-05-31 18:55:05 -0300

    First commit

---
+++ Curso API con .Net/Migrations/20230531193719_First Migration-0001.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Curso_API_con_.Net.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration0001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(280)", maxLength: 280, nullable: false),
+                    Target = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Requirements = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Curso API con .Net/Models/DataModels/Course.cs
@@ -0,0 +1,23 @@
+﻿using Curso_API_con_.Net.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Curso_API_con_.Net.Models.DataModels
+{
+    public class Course : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        [Required, StringLength(280)]
+        public string Description { get; set; }
+
+        [Required]
+        public string Target { get; set; }
+
+        [Required]
+        public string Requirements { get; set; }
+
+        [Required]
+        public Levels Level { get; set; }
+    }
+}
